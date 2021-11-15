dc3f27b7a090c7ed571cacba85d3dbcce90fee85
linus <gudgeon.christopher@gmail.com>
2021-11-15 21:54:08 +0000

    Add grid lines

--- Hud.cs
+++ Hud.cs
@@ -10,6 +10,11 @@ namespace chips {
 
         }
         public static void Draw(SpriteBatch spriteBatch) {
+            const int space = 100;
+            for (int i = (int)(Camera.ScreenToWorld(Vector2.Zero).X - Camera.ScreenToWorld(Vector2.Zero).X % space); i < Camera.ScreenToWorld(GameRoot.ScreenSize).X + space - Camera.ScreenToWorld(GameRoot.ScreenSize).X % space; i += space)
+                spriteBatch.Draw(Art.Pixel, new Rectangle((int)Camera.WorldToScreen(new Vector2(i, 0)).X, 0, 1, (int)GameRoot.ScreenSize.Y), Color.LightBlue);
+            for (int i = (int)(Camera.ScreenToWorld(Vector2.Zero).Y - Camera.ScreenToWorld(Vector2.Zero).Y % space); i < Camera.ScreenToWorld(GameRoot.ScreenSize).Y + space - Camera.ScreenToWorld(GameRoot.ScreenSize).Y % space; i += space)
+                spriteBatch.Draw(Art.Pixel, new Rectangle(0, (int)Camera.WorldToScreen(new Vector2(0, i)).Y, (int)GameRoot.ScreenSize.X, 1), Color.LightBlue);
             spriteBatch.Draw(Art.Hud, Vector2.Zero, Color.White);
         }
     }
