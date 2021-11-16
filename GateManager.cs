2e51b726fb850dad71597fe7c897ccd08b3a0bd2
LinesNya <60029482+LinesGuy@users.noreply.github.com>
2021-11-16 12:28:32 +0000

    Move GateManager and Wire classes to their own files

---
+++ GateManager.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace chips {
+    public static class GateManager {
+        public static List<Gate> Gates = new List<Gate>();
+        public static List<Wire> Wires = new List<Wire>();
+        private static string selectedType = "none";
+        private static int selectedIndex;
+        public static void Update() {
+            // Updates gates and wires
+            foreach (Gate gate in Gates)
+                gate.Update();
+            foreach (Wire wire in Wires)
+                wire.Update();
+            Gates = Gates.Where(g => !g.isExpired).ToList();
+            Wires = Wires.Where(w => !w.isExpired).ToList();
+            if (Input.WasLeftButtonJustDown()) {
+                // Default to camera
+                selectedType = "camera";
+                // Check wire start positions
+                for (int i = 0; i < Wires.Count; i++) {
+                    Wire wire = Wires[i];
+                    if (Vector2.DistanceSquared(wire.StartPosition, Camera.MouseWorldCoords()) < Wire.Radius * Wire.Radius) {
+                        selectedIndex = i;
+                        selectedType = "wireStart";
+                    }
+                }
+                // Gates
+                for (int i = 0; i < Gates.Count; i++) {
+                    Gate gate = Gates[i];
+                    // Check if moving gate
+                    if (gate.Rect.Contains(Input.mouse.Position)) {
+                        if (gate.Type == "switch") {
+                            gate.Type = "switchClosed";
+                            gate.Image = Art.SwitchClosed;
+                        } else if (gate.Type == "switchClosed") {
+                            gate.Type = "switch";
+                            gate.Image = Art.Switch;
+                        } else if (gate.Type == "buttonOn") {
+                            gate.Type = "buttonOff";
+                            gate.Image = Art.ButtonOff;
+                        } else if (gate.Type == "buttonOff") {
+                            gate.Type = "buttonOn";
+                            gate.Image = Art.ButtonOn;
+                        }
+                        selectedIndex = i;
+                        selectedType = "gate";
+                    }
+                    // Check if adding wire from input
+                    foreach (Vector2 pos in gate.OutputPositions) {
+                        if (Vector2.DistanceSquared(pos, Camera.MouseWorldCoords()) < Wire.Radius * Wire.Radius) {
+                            Add(new Wire(pos, Camera.MouseWorldCoords()));
+                            selectedIndex = Wires.Count - 1;
+                            selectedType = "wireEnd";
+                        }
+                    }
+                }
+                // Check wire end positions
+                for (int i = 0; i < Wires.Count; i++) {
+                    Wire wire = Wires[i];
+                    if (Vector2.DistanceSquared(wire.EndPosition, Camera.MouseWorldCoords()) < Wire.Radius * Wire.Radius) {
+                        selectedIndex = i;
+                        selectedType = "wireEnd";
+                    }
+                }
+            } else if (Input.WasLeftButtonJustUp()) {
+                if (selectedType == "wireEnd") {
+                    foreach (Gate gate in Gates) {
+                        foreach (Vector2 inputPos in gate.InputPositions) {
+                            if (Vector2.DistanceSquared(Wires[selectedIndex].EndPosition, inputPos) < Wire.Radius * Wire.Radius) {
+                                Wires[selectedIndex].EndPosition = inputPos;
+                            }
+                        }
+                    }
+                }
+                if (selectedType == "wireStart") {
+                    foreach (Gate gate in Gates) {
+                        foreach (Vector2 outputPos in gate.OutputPositions) {
+                            if (Vector2.DistanceSquared(Wires[selectedIndex].StartPosition, outputPos) < Wire.Radius * Wire.Radius) {
+                                Wires[selectedIndex].StartPosition = outputPos;
+                            }
+                        }
+                    }
+                }
+                selectedType = "none";
+
+            }
+            switch (selectedType) {
+                case "camera":
+                    Camera.CameraPosition -= Input.DeltaMouse();
+                    break;
+                case "gate":
+                    Gate gate = Gates[selectedIndex];
+                    foreach (Vector2 inputPos in gate.InputPositions) {
+                        foreach (Wire wire in Wires) {
+                            if (wire.EndPosition == inputPos) {
+                                wire.EndPosition += Input.DeltaMouse();
+                            }
+                        }
+                    }
+                    foreach (Vector2 outputPos in gate.OutputPositions) {
+                        foreach (Wire wire in Wires) {
+                            if (wire.StartPosition == outputPos) {
+                                wire.StartPosition += Input.DeltaMouse();
+                            }
+                        }
+                    }
+                    Gates[selectedIndex].Position += Input.DeltaMouse();
+                    Gates[selectedIndex].UpdateRect();
+                    break;
+                case "wireStart":
+                    Wires[selectedIndex].StartPosition += Input.DeltaMouse();
+                    break;
+                case "wireEnd":
+                    Wires[selectedIndex].EndPosition += Input.DeltaMouse();
+                    break;
+                default:
+                    break;
+            }
+            // Create new objects
+            if (Input.WasKeyJustDown(Keys.D0))
+                Add(new Wire(Camera.MouseWorldCoords(), Camera.MouseWorldCoords() + new Vector2(100, 0)));
+            if (Input.WasKeyJustDown(Keys.D1))
+                Add(new Gate(Camera.MouseWorldCoords(), "and"));
+            if (Input.WasKeyJustDown(Keys.D2))
+                Add(new Gate(Camera.MouseWorldCoords(), "or"));
+            if (Input.WasKeyJustDown(Keys.D3))
+                Add(new Gate(Camera.MouseWorldCoords(), "nand"));
+            if (Input.WasKeyJustDown(Keys.D4))
+                Add(new Gate(Camera.MouseWorldCoords(), "nor"));
+            if (Input.WasKeyJustDown(Keys.D5))
+                Add(new Gate(Camera.MouseWorldCoords(), "xor"));
+            if (Input.WasKeyJustDown(Keys.D6))
+                Add(new Gate(Camera.MouseWorldCoords(), "not"));
+            if (Input.WasKeyJustDown(Keys.D7))
+                Add(new Gate(Camera.MouseWorldCoords(), "one"));
+            if (Input.WasKeyJustDown(Keys.D8))
+                Add(new Gate(Camera.MouseWorldCoords(), "led"));
+            if (Input.WasKeyJustDown(Keys.D9))
+                Add(new Gate(Camera.MouseWorldCoords(), "switch"));
+            if (Input.WasKeyJustDown(Keys.B))
+                Add(new Gate(Camera.MouseWorldCoords(), "buttonOn"));
+        }
+        public static void Draw(SpriteBatch spriteBatch) {
+            foreach (Gate gate in Gates)
+                gate.Draw(spriteBatch);
+            foreach (Wire wire in Wires)
+                wire.Draw(spriteBatch);
+        }
+        public static void Add(Gate gate) {
+            Gates.Add(gate);
+        }
+        public static void Add(Wire wire) {
+            Wires.Add(wire);
+        }
+    }
+}
---
+++ Wire.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chips {
+    public class Wire {
+        public Vector2 StartPosition;
+        public Vector2 EndPosition;
+        public const float Radius = 10f;
+        public bool activated = false;
+        public bool isExpired = false;
+        public Wire(Vector2 startPosition, Vector2 endPosition) {
+            StartPosition = startPosition;
+            EndPosition = endPosition;
+        }
+        public float DistanceFromPoint(Vector2 point) {
+            float m = (EndPosition.Y - StartPosition.Y) / (EndPosition.X - StartPosition.X);
+            return MathF.Abs(-m * point.X + point.Y + m * StartPosition.X - StartPosition.Y) / MathF.Sqrt(MathF.Pow(m, 2) + 1);
+        }
+        public void Update() {
+            activated = false;
+            foreach (Gate gate in GateManager.Gates)
+                for (int i = 0; i < gate.Outputs.Length; i++)
+                    if (gate.Outputs[i] && Vector2.DistanceSquared(gate.OutputPositions[i], StartPosition) < Radius * Radius)
+                        activated = true;
+            if (Input.WasRightButtonJustDown() && DistanceFromPoint(Camera.MouseWorldCoords()) < 10f && new Rectangle((int)Math.Min(EndPosition.X, StartPosition.X) - 5, (int)Math.Min(EndPosition.Y, StartPosition.Y) - 5, (int)Math.Abs(EndPosition.X - StartPosition.X) + 5, (int)Math.Abs(EndPosition.Y - StartPosition.Y) + 5).Contains(Camera.MouseWorldCoords()))
+                isExpired = true; // If wire was just right clicked, delete it
+        }
+        public void Draw(SpriteBatch spriteBatch) {
+            Vector2 StartScreen = Camera.WorldToScreen(StartPosition);
+            Vector2 EndScreen = Camera.WorldToScreen(EndPosition);
+            // End point
+            spriteBatch.Draw(Art.Pixel, StartScreen, null, activated ? Color.Red : Color.Green, 0f, new Vector2(0.5f), Radius, 0, 0);
+            // Start point
+            spriteBatch.Draw(Art.Pixel, EndScreen, null, activated ? Color.Red : Color.Green, 0f, new Vector2(0.5f), Radius, 0, 0);
+            // Line
+            spriteBatch.Draw(Art.Pixel, StartScreen, null, activated ? Color.DarkRed : Color.Black, MathF.Atan2(EndScreen.Y - StartScreen.Y, EndScreen.X - StartScreen.X), new Vector2(0, 0.5f), new Vector2(MathF.Sqrt(MathF.Pow(EndScreen.X - StartScreen.X, 2) + MathF.Pow(EndScreen.Y - StartScreen.Y, 2)), 3f * Camera.Zoom), 0, 0); ;
+            // Arrow
+            spriteBatch.Draw(Art.Arrow, (StartScreen + EndScreen) / 2f, null, Color.Black, MathF.Atan2(EndScreen.Y - StartScreen.Y, EndScreen.X - StartScreen.X), new Vector2(Art.Arrow.Width, Art.Arrow.Height) / 2f, Camera.Zoom / 2f, 0, 0); ;
+        }
+    }
+}
