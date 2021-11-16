2a41c6a425b568f36977a3299e02ce0e13657cea
LinesNya <60029482+LinesGuy@users.noreply.github.com>
2021-11-16 12:24:31 +0000

    Change gate input/output node colours based on activation

--- Gate.cs
+++ Gate.cs
@@ -172,14 +172,14 @@ namespace chips
             spriteBatch.Draw(Image, Rect, Color.White);
             // Draw inputs (either one or two)
             if (Inputs.Length == 1) {
-                spriteBatch.Draw(Art.Pixel, Camera.WorldToScreen(new Vector2(Position.X - Size.X / 2f, Position.Y)), null, Color.Green, 0f, new Vector2(0.5f), 10f, 0, 0);
+                spriteBatch.Draw(Art.Pixel, Camera.WorldToScreen(new Vector2(Position.X - Size.X / 2f, Position.Y)), null, Inputs[0] ? Color.Red : Color.Green, 0f, new Vector2(0.5f), 10f, 0, 0);
             } else if (Inputs.Length == 2) {
-                spriteBatch.Draw(Art.Pixel, Camera.WorldToScreen(InputPositions[0]), null, Color.Green, 0f, new Vector2(0.5f), 10f, 0, 0);
-                spriteBatch.Draw(Art.Pixel, Camera.WorldToScreen(InputPositions[1]), null, Color.Green, 0f, new Vector2(0.5f), 10f, 0, 0);
+                spriteBatch.Draw(Art.Pixel, Camera.WorldToScreen(InputPositions[0]), null, Inputs[0] ? Color.Red : Color.Green, 0f, new Vector2(0.5f), 10f, 0, 0);
+                spriteBatch.Draw(Art.Pixel, Camera.WorldToScreen(InputPositions[1]), null, Inputs[1] ? Color.Red : Color.Green, 0f, new Vector2(0.5f), 10f, 0, 0);
             }
             // Draw output (if there is one)
             if (Outputs.Length == 1)
-                spriteBatch.Draw(Art.Pixel, Camera.WorldToScreen(new Vector2(Position.X + Size.X / 2f, Position.Y)), null, Color.Green, 0f, new Vector2(0.5f), 10f, 0, 0);
+                spriteBatch.Draw(Art.Pixel, Camera.WorldToScreen(new Vector2(Position.X + Size.X / 2f, Position.Y)), null, Outputs[0] ? Color.Red : Color.Green, 0f, new Vector2(0.5f), 10f, 0, 0);
             // If gate is an LED, draw whether it's on or off
             if (Type == "led" && Inputs[0] == true) {
                 spriteBatch.Draw(Art.LedOn, Rect, Color.White);
